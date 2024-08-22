511cc3cc6ff23bc40d1bfab2e020313224bb525c
x2d7751347m <62936128+x2d7751347m@users.noreply.github.com>
2024-08-22 16:16:00 +0900

    add dapper and ef

---
+++ DotNetAPI/Data/DataContextDapper.cs
@@ -0,0 +1,43 @@
+﻿using System.Data;
+using Dapper;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace DotNetAPI.Data;
+
+public class DataContextDapper
+{
+    private readonly IConfiguration _config;
+    public DataContextDapper(IConfiguration config)
+    {
+        _config = config;
+    }
+
+    public IEnumerable<T> LoadData<T>(string sql)
+    {
+        using (IDbConnection dbConnection = new SqlConnection(_config.GetConnectionString("DefaultConnection")))
+        {
+            dbConnection.Open();
+            using (IDbTransaction tran = dbConnection.BeginTransaction(IsolationLevel.ReadCommitted))
+            {
+                var holdVal = dbConnection.Query<T>(sql, null, transaction: tran, commandTimeout: 999999999);
+                dbConnection.Close();
+                return holdVal;
+            }
+        }
+    }
+
+    public int ExecuteSql(string sql)
+    {
+        using (IDbConnection dbConnection = new SqlConnection(_config.GetConnectionString("DefaultConnection")))
+        {
+            return dbConnection.Execute(sql);
+        }
+    }
+
+    public void ExecuteSqlMulti(string sql, IDbConnection dbConnection)
+    {
+        dbConnection.Execute(sql);
+    }
+
+}
--- DotNetAPI/obj/Debug/net8.0/DotNetAPI.AssemblyInfo.cs
+++ DotNetAPI/obj/Debug/net8.0/DotNetAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotNetAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33d2733751eb1175fc97c3b995320c37970c08d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotNetAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotNetAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
